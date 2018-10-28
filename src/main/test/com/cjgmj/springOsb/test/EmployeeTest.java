17edf9c41e4f0d8e8c6abea2e0cd718b360a9390
cjgmj <cjgmj@hotmail.com>
2018-10-28 15:02:28 +0100

    Test con seguridad

--- src/main/test/com/cjgmj/springOsb/test/EmployeeTest.java
+++ src/main/test/com/cjgmj/springOsb/test/EmployeeTest.java
@@ -1,6 +1,9 @@
 package com.cjgmj.springOsb.test;
 
+import java.util.Arrays;
+
 import org.apache.log4j.Logger;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.http.HttpEntity;
@@ -15,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.cjgmj.springOsb.entity.Employee;
 import com.google.gson.Gson;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:applicationContextTest.xml" })
@@ -23,8 +27,48 @@ public class EmployeeTest {
 	private static final Logger LOG = Logger.getLogger(EmployeeTest.class);
 
 	private static final String ENDPOINT = "http://localhost:7101/EmployeeRest/Proxies/PipelineRestEmployee-proxy";
+	private static final String URL = "http://localhost:7101/springOsb";
+
+	private static HttpHeaders getHeaders() {
+		String plainCredentials = "alex:54321";
+		String base64Credentials = new String(Base64.encode(plainCredentials.getBytes()));
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Basic " + base64Credentials);
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		return headers;
+	}
 
 	@Test
+	public void getOneEmployee() {
+		try {
+			HttpEntity<String> request = new HttpEntity<>(getHeaders());
+			String uri = URL + "/employee/getEmployee/{id}";
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<Employee> responseEmployee = restTemplate.exchange(uri, HttpMethod.GET, request,
+					Employee.class, "1");
+			LOG.info("Name object Employee: " + responseEmployee.getBody().getName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testList() {
+		try {
+			HttpEntity<String> request = new HttpEntity<>(getHeaders());
+			String uri = URL + "/employee/getEmployee";
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<Employee[]> responseEmployee = restTemplate.exchange(uri, HttpMethod.GET, request,
+					Employee[].class);
+			for(Employee emp: responseEmployee.getBody())
+				LOG.info("Employee: " + emp.getName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	@Ignore
 	public void testWSRest() {
 		try {
 
@@ -44,6 +88,7 @@ public class EmployeeTest {
 	}
 
 	@Test
+	@Ignore
 	public void testAddEmployee() {
 		try {
 
